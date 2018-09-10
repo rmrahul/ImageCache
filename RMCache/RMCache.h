8a06c8f1df5b881b85064de950a527696d523ef3
rmrahul <rahulmane3089@gmail.com>
2018-09-10 09:25:50 +0530

    Initial commit by rahul

---
+++ RMCache/RMCache.h
@@ -0,0 +1,19 @@
+//
+//  RMCache.h
+//  RMCache
+//
+//  Created by Rahul Mane on 09/09/18.
+//  Copyright © 2018 developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RMCache.
+FOUNDATION_EXPORT double RMCacheVersionNumber;
+
+//! Project version string for RMCache.
+FOUNDATION_EXPORT const unsigned char RMCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RMCache/PublicHeader.h>
+
+
